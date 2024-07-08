6a8dcb62709e8174c82a954ce89d9ffb4a0bab5f
TEJAS-WADPILLEWAR <tejaswadpillewar2@gmail.com>
2024-07-08 14:45:13 +0530

    utils code updated

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 
 dotenv.config({
-    path: './env'
-})
-
+  path: "./env",
+});
 
 connectDB()
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection failed !!", err);
+  });
 
 // import express from "express"
 
@@ -26,8 +31,7 @@ connectDB()
 
 //        app.listen(process.env.PORT, () => {
 //         console.log(`App is listening on port ${process.env.PORT}`);
-//        }), 
-
+//        }),
 
 //     } catch (error) {
 //         console.log("ERROR: ", error);
