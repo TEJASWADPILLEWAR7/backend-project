9df523c8952052021ac8ec87792d7600a549b832
TEJAS-WADPILLEWAR <tejaswadpillewar2@gmail.com>
2024-07-09 19:53:39 +0530

    Correction in ApiError

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -29,7 +29,7 @@ const userSchema = new Schema(
       type: String, // cloudinary url
       required: true,
     },
-    coberImage: {
+    coverImage: {
       type: String, // cloudinary url
     },
     watchHistory: [
